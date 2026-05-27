0966d70d4b56744e6ca47edf43f4f40d5a931be0
Pierre918 <kasparianpierre@gmail.com>
2026-05-27 09:39:58 +0200

    feat: add images to projects

--- app/[lang]/projects/page.tsx
+++ app/[lang]/projects/page.tsx
@@ -67,7 +67,7 @@ export default async function ProjectsPage({
                           src={project.imagePrincipale}
                           alt={project.title[lang]}
                           fill
-                          priority={idx < 3}
+                          priority={idx < 6}
                           className="object-cover transition-transform duration-300 group-hover:scale-105"
                           sizes="(max-width: 768px) 100vw, (max-width: 1024px) 50vw, 33vw"
                         />
